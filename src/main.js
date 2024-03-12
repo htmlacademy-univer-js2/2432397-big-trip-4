a8f0151f757f2ca514ef227ef60ee27cd9af7031
koborochka <098mishawifi@gmail.com>
2024-03-12 22:30:57 +0500

    добавил и подключил модель

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import TripPresenter from './presenter/trip-presenter';
+import PointsModel from './model/points-model.js';
 
-const tripPresenter = new TripPresenter();
+const pointsModel = new PointsModel();
+const tripPresenter = new TripPresenter(pointsModel);
 tripPresenter.init();
