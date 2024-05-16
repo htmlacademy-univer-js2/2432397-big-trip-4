1aefc3da33531e2df292e0bb4046f29c505a64f6
koborochka <098mishawifi@gmail.com>
2024-05-16 23:00:10 +0500

    сортировка по дням, времени и цене

--- src/template/sort-template.js
+++ src/template/sort-template.js
@@ -1,30 +1,35 @@
-export function createSortTemplate(){
-  return `
-    <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+import { SortTypes } from '../const';
+
+export function createSortTemplate(sortType){
+  return `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
       <div class="trip-sort__item  trip-sort__item--day">
-        <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked>
+        <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day"
+          data-sort-type="${SortTypes.DAY}" ${sortType === SortTypes.DAY ? 'checked' : ''}>
         <label class="trip-sort__btn" for="sort-day">Day</label>
       </div>
 
       <div class="trip-sort__item  trip-sort__item--event">
-        <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" disabled>
+        <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event"
+          data-sort-type="${SortTypes.EVENT}" disabled>
         <label class="trip-sort__btn" for="sort-event">Event</label>
       </div>
 
       <div class="trip-sort__item  trip-sort__item--time">
-        <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
+        <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time"
+          data-sort-type="${SortTypes.TIME}" ${sortType === SortTypes.TIME ? 'checked' : ''}>
         <label class="trip-sort__btn" for="sort-time">Time</label>
       </div>
 
       <div class="trip-sort__item  trip-sort__item--price">
-        <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
+        <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price"
+          data-sort-type="${SortTypes.PRICE}" ${sortType === SortTypes.PRICE ? 'checked' : ''}>
         <label class="trip-sort__btn" for="sort-price">Price</label>
       </div>
 
       <div class="trip-sort__item  trip-sort__item--offer">
-        <input id="sort-offer" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-offer" disabled>
+        <input id="sort-offer" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-offer"
+          data-sort-type="${SortTypes.OFFERS}" disabled>
         <label class="trip-sort__btn" for="sort-offer">Offers</label>
       </div>
-    </form>
-  `;
+    </form>`;
 }
--- src/utils.js
+++ src/utils.js
@@ -19,4 +19,28 @@ function getTimeInMinutes(startTime, endTime) {
 function updateItem(items, update) {
   return items.map((item) => item.id === update.id ? update : item);
 }
-export { getRandomArrayElement, getRandomUUID, getTimeInHours, getTimeInMinutes, updateItem};
+
+function sortPointDay(points) {
+  return points.sort((firstPoint, secondPoint) => new Date(firstPoint.dateFrom) - new Date(secondPoint.dateTo));
+}
+
+function sortPointTime(points) {
+  return points.sort((firstPoint, secondPoint) =>
+    dayjs(firstPoint.dateFrom).diff(dayjs(firstPoint.dateTo), 'minutes') -
+    dayjs(secondPoint.dateFrom).diff(dayjs(secondPoint.dateTo), 'minutes'));
+}
+
+function sortPointPrice(points) {
+  return points.sort((firstPoint, secondPoint) => secondPoint.basePrice - firstPoint.basePrice);
+}
+
+export {
+  getRandomArrayElement,
+  getRandomUUID,
+  getTimeInHours,
+  getTimeInMinutes,
+  updateItem,
+  sortPointTime,
+  sortPointPrice,
+  sortPointDay,
+};
